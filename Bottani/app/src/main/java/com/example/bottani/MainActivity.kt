09c7af8de7218e56e85945bdace1497c025a9e3b
iammobarok <iammobarok@gmail.com>
2019-12-14 16:07:01 +0700

    fix bottomnavbar

--- Bottani/app/src/main/java/com/example/bottani/MainActivity.kt
+++ Bottani/app/src/main/java/com/example/bottani/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.bottani
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Get Navbar
+
+        navController = Navigation.findNavController(this, R.id.fragment)
+
+        //Setting the navigation controller to Bottom Nav
+        bottomNav.setupWithNavController(navController)
+
+
+        //Setting up the action bar
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
--- Bottani/app/src/main/java/com/example/bottani/Plants.kt
+++
@@ -1,4 +0,0 @@
-package com.example.bottani
-
-class Plants {
-}
