a7fbf10fb5f31f94747af468826bc974207dc884
iammobarok <iammobarok@gmail.com>
2019-12-14 17:55:46 +0700

    fix bottom navbar

---
+++ Bottani/app/src/main/java/com/example/bottani/FragmenAccount.kt
@@ -0,0 +1,21 @@
+package com.example.bottani
+
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+
+
+class FragmenAccount : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_account, container, false)
+    }
+}
